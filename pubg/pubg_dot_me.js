2bc42cf68fbb8583709b7c6d947f137ea6ca91dc
Homer Huang <aclisp@gmail.com>
2017-11-26 01:12:56 +0800

    Get more stats.

--- pubg/pubg_dot_me.js
+++ pubg/pubg_dot_me.js
@@ -2,26 +2,44 @@ var logger = require('./logger')('pubg_dot_me')
 var superagent = require('superagent')
 var cheerio = require('cheerio')
 var record = {}
+var baseUrl = 'https://pubg.me'
 
 function get (playerName) {
   superagent
-    .get('https://pubg.me/player/' + playerName)
+    .get(baseUrl + '/player/' + playerName)
     .end(function (err, res) {
       if (err) {
         logger.error('player `' + playerName + '`: ' + err)
         return
       }
       var $ = cheerio.load(res.text)
-      parse($)
+      parse($, playerName)
     })
 }
 
-function parse ($) {
+function parse ($, playerName) {
+  record.baseUrl = baseUrl
+  record.playerName = playerName
   $('.last-updated').each(function (i, elem) {
     record.lastUpdated = $(this).text()
+  })
+  $('.profile-header-stats.d-flex.flex-column .stat .value').each(function (i, item) {
+    if (i === 0) {
+      record.wins = $(this).text()
+    } else if (i === 1) {
+      record.matches = $(this).text()
+    } else if (i === 2) {
+      record.kills = $(this).text()
+    } else if (i === 3) {
+      record.timeSpentPlaying = $(this).text()
+    }
+  })
+  record.stats = []
+  $('.dropdown-menu-profile-stats .dropdown-item.d-flex').each(function (i, item) {
+    record.stats.push($(this).attr('href'))
   })
   logger.info('[parse] record = ' + JSON.stringify(record, null, 2))
 }
 
-get('Staver')
+get('Gidroplan')
 logger.info('[main] record = ' + JSON.stringify(record, null, 2))
